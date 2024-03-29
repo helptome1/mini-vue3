fe6e59f281db529a0252ea1ec6b22423f039f20e
helptome1 <hzgdeemail@163.com>
2024-03-29 23:18:23 +0800

    feat：effect

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,15 +1,36 @@
+import { extend } from '../shared'
+
 class ReactiveEffect {
   private _fn: any
-
-  constructor(fn) {
+  deps = []
+  active = true
+  onStop?: () => void
+  public scheduler: Function | undefined
+  constructor(fn, scheduler?: Function) {
     this._fn = fn
+    this.scheduler = scheduler
   }
-
   run() {
     // 获取当前执行的effect实例
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this)
+      if (this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+
+// 清除当前effect
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 const targetMap = new Map()
@@ -26,21 +47,38 @@ export function track(target, key) {
     dep = new Set()
     depsMap.set(key, dep)
   }
+  if (!activeEffect) return
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
+// 触发依赖
 export function trigger(target, key) {
   const depsMap = targetMap.get(target)
   const deps = depsMap.get(key)
-  for (let dep of deps) {
-    dep.run()
+  for (let effect of deps) {
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
   }
 }
 
 // 定义一个全局变量进行获取当前的effect
 let activeEffect
-export function effect(fn) {
+export function effect(fn, options: any = {}) {
   // fn
-  const _effect = new ReactiveEffect(fn)
+  const _effect = new ReactiveEffect(fn, options.scheduler)
+  // extend
+  extend(_effect, options)
+
   _effect.run()
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
+}
+
+export function stop(runner) {
+  runner.effect.stop()
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
-import { effect } from "../effect"
-import { reactive } from "../reactive"
+import { effect, stop } from '../effect'
+import { reactive } from '../reactive'
 
 describe('effect', () => {
   it('start effect', () => {
@@ -18,5 +18,84 @@ describe('effect', () => {
     // update
     user.age++
     expect(nextAge).toBe(12)
+  })
+  // effect返回里面包裹的函数
+  it('effect return', () => {
+    // 1. effect(fn) -> function(runner) -> fn -> return
+    let foo = 10
+    const runner = effect(() => {
+      foo = foo + 1
+      return 'foo'
+    })
+    expect(foo).toBe(11)
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe('foo')
+  })
+  // scheduler是实现方式
+  // 1. 通过effect 的第二个参数给定一个scheduler的fn
+  // 2. effect第一次执行的时候，还会执行fn
+  // 3. 当响应式对象set update不会执行fn 而是执行scheduler
+  // 4. 如果说当执行runner的时候，会再次的执行fn
+  it('scheduler', () => {
+    let dummy
+    let run: any
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({ foo: 1 })
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      { scheduler }
+    )
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    // should be called on first trigger
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // should not run yet
+    expect(dummy).toBe(1)
+    // manually run
+    run()
+    // should have run
+    expect(dummy).toBe(2)
+  })
+
+  it('stop', () => {
+    let dummy
+    const obj = reactive({ prop: 1 })
+    const runner = effect(() => {
+      dummy = obj.prop
+    })
+    obj.prop = 2
+    expect(dummy).toBe(2)
+    stop(runner)
+    obj.prop = 3
+    expect(dummy).toBe(2)
+
+    // stopped effect should still be manually callable
+    runner()
+    expect(dummy).toBe(3)
+  })
+
+  it('events: onStop', () => {
+    const obj = reactive({
+      foo: 1
+    })
+    const onStop = jest.fn()
+    let dummy
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      {
+        onStop
+      }
+    )
+
+    stop(runner)
+    expect(onStop).toHaveBeenCalledTimes(1)
   })
 })
